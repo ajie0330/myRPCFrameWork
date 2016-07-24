1f66d3ae8a15c62a1deff0bce6a5b813c18413dd
赵忠杰 <zhaozhongjie@192.168.0.103>
2016-07-24 16:31:15 +0800

    hello world test

---
+++ rpc-common/src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by zhaozhongjie on 16/7/24.
+ */
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.print("------");
+    }
+}
